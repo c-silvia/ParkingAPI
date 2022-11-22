014c1fccf5f1dcd64cd96adec5f41e24dc8d8426
c-silvia <35904551+c-silvia@users.noreply.github.com>
2022-11-22 13:33:44 +0100

    Database creation queries added.

--- tests/test_db.py
+++ tests/test_db.py
@@ -5,6 +5,8 @@ from unittest.mock import patch, MagicMock
 
 import parking_app.db
 from parking_app.db import DBClient, DBUsers, DBData
+import parking_app.exceptions
+from parking_app.exceptions import UserNotFound
 
 
 @patch("parking_app.db._connect_to_db")  # mocking something from another file
@@ -37,16 +39,20 @@ class TestDBClient(TestCase):
 @patch("parking_app.db._connect_to_db")  # mocking something from another file
 class TestDBUsers(TestCase):
 
-    def test_user_creation(self, db_connector_function):
-        db_user = DBUsers()
-        #("INSERT INTO login_data (username, email_address, password) "VALUES (%s, %s, %s);", ["a_username", "email@address.com", "Pa55wor!"])
+    # def test_user_creation(self, db_connector_function):
+    #     db_user = DBUsers()
+    #     cursor = MagicMock()
+        # ("INSERT INTO login_data (username, email_address, password) "VALUES (%s, %s, %s);", ["a_username", "email@address.com", "Pa55wor!"])
 
-    def test_check_for_non_existing_user(self, db_connector_function):
+    def test_check_for_existing_user_negative(self, db_connector_function):
         db_user = DBUsers()
+        cursor = MagicMock()
+        cursor.__iter__.return_value = [()]
+        db_user.cnx.cursor.return_value.__enter__.return_value = cursor
         result = db_user.check_if_username_already_exists("a_username")
         self.assertEqual(False, result)
 
-    def test_check_for_existing_user(self, db_connector_function):
+    def test_check_for_existing_user_positive(self, db_connector_function):
         db_user = DBUsers()
         cursor = MagicMock()
         cursor.__iter__.return_value = [(1,)]
@@ -54,11 +60,42 @@ class TestDBUsers(TestCase):
         result = db_user.check_if_username_already_exists("a_username")
         self.assertEqual(True, result)
 
-    def test_check_if_email_address_already_exists(self, db_connector_function):
+    def test_check_for_existing_address_negative(self, db_connector_function):
         db_user = DBUsers()
-        result = db_user.check_if_username_already_exists("address@email.com")
+        cursor = MagicMock()
+        cursor.__iter__.return_value = [()]
+        db_user.cnx.cursor.return_value.__enter__.return_value = cursor
+        result = db_user.check_if_email_address_already_exists("address@email.com")
         self.assertEqual(False, result)
 
+    # not working
+    def test_check_for_existing_address_positive(self, db_connector_function):
+        db_user = DBUsers()
+        cursor = MagicMock()
+        cursor.__iter__.return_value = [(1,)]
+        db_user.cnx.cursor.return_value.__enter__.return_value = cursor
+        result = db_user.check_if_email_address_already_exists("address@email.com")
+        self.assertEqual(True, result)
 
-class MockDBData(TestCase):
+    def test_getting_user_data_from_username_negative(self, db_connector_function):
+        db_user = DBUsers()
+        cursor = MagicMock()
+        cursor.__iter__.return_value = [()]
+        db_user.cnx.cursor.return_value.__enter__.return_value = cursor
+        with self.assertRaises(parking_app.exceptions.UserNotFound):
+            db_user.get_user_data_from_username("Beethoven01")
+
+    # not working
+    def test_getting_user_data_from_username_positive(self, db_connector_function):
+        db_user = DBUsers()
+        cursor = MagicMock()
+        cursor.__iter__.return_value = [(1, "Beethoven01", "address@email.com", "Pa55wor!",)]
+        db_user.cnx.cursor.return_value.__enter__.return_value = cursor
+        result = db_user.get_user_data_from_username("Beethoven01")
+        self.assertEqual(
+            {"user_id": 1, "username": "Beethoven01", "email_address": "address@email.com", "password": "Pa55wor!"},
+            result)
+
+
+class TestDBData(TestCase):
     pass
